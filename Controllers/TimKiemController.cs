192130b7018ca375c4c4a3eee1a6b84132ae281f
nhuanh9 <chopperlouis@gmail.com>
2020-05-06 16:13:22 +0700

    pre paging for search

--- Controllers/TimKiemController.cs
+++ Controllers/TimKiemController.cs
@@ -12,19 +12,22 @@ namespace BTL.Controllers
     {
         MobileStoreManagementEntities db = new MobileStoreManagementEntities();
         // GET: TimKiem
-        public ActionResult KetQuaTimKiem(FormCollection form)
+        public ActionResult KetQuaTimKiem(FormCollection form, int? page)
         {
+            int pageSize = 100;
+            int pageNumber = (page ?? 1);
             string searchKey = form["txtTimKiem"].ToString();
+            ViewBag.form = form;
             List<SanPham> lstKQTK = db.SanPhams.Where(n => n.TenSP.Contains(searchKey)).ToList();
             if (lstKQTK.Count == 0)
             {
                 ViewBag.ThongBao = "Không tìm thấy sản phẩm nào!";
-                return View(db.SanPhams.ToList());
+                return View(db.SanPhams.ToList().ToPagedList(pageNumber, pageSize));
             }
             else
             {
-                ViewBag.ThongBao = "Đã tìm thấy " + lstKQTK.Count() + " sản phẩm "+ searchKey;
-                return View(lstKQTK.ToList());
+                ViewBag.ThongBao = "Đã tìm thấy " + lstKQTK.Count() + " sản Phẩm "+searchKey;
+                return View(lstKQTK.ToList().ToPagedList(pageNumber, pageSize));
             }
         }
     }
